6bd0be46a8c987095b157844e1e3444ff0a97bcf
Setric muholo <muholosetric@gmail.com>
2023-01-21 05:46:15 +0000

    fixed error on task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    """if number == 98:
-        print("{}".format(number))
-    else:
-    """
     print("{:02}".format(number), end=", ")
 print()
